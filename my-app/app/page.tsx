b7be122809d2f15ed00e34f07073dbfc686d73cc
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-08-11 18:29:55 +0530

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,51 +1,35 @@
-'use client'
+'use client';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import Image from "next/image";
 import Hero from "./components/Hero";
 import { FloatingNav } from "./components/NavBar";
-import { FaHome } from "react-icons/fa";
 import Grid from "./components/Grid";
 import RecentProjects from "./components/RecentProjects";
-import Clients from "./components/Clients";
-import { navItems } from "@/data";
 import Experience from "./components/Experience";
 import Approach from "./components/Approach";
 import Footer from "./components/Footer";
 import PassKeyModel from "./components/PassKey";
+import { navItems } from "@/data";
 
-import { useEffect, useState } from 'react';
-import { useRouter } from "next/navigation";
-import { usePathname } from "next/navigation";
-import { getWorkExperience } from "@/app/lib/actions/admin.actions";
-import { useSearchParams } from 'next/navigation'
-
-declare type SearchParamProps = {
-  params: { [key: string]: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export default function Home() {
-  const router = useRouter();
+export default function Home({ searchParams }: any) {
   const [isAdmin, setIsAdmin] = useState(false);
-  const pathname=usePathname();
-  const searchParams = useSearchParams();
-   
-  //const isAdmin=searchParams?.admin==="true";
+  const router = useRouter();
+  const pathname = usePathname();
 
   useEffect(() => {
-    const admin = searchParams.get('admin') == "true";
-    console.log("typeof admin",typeof admin)
-    console.log("admin",admin)
-    console.log("searchParams?.admin",searchParams.get('admin'))
-    setIsAdmin(admin);
-  }, [isAdmin, pathname,searchParams]);
-
-
+    if (typeof window !== 'undefined') { // Ensures this runs only on the client
+      const admin = searchParams?.admin === "true";
+      setIsAdmin(admin);
+    }
+  }, [searchParams, pathname]);
 
   return (
     <main className="relative bg-black justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-[100vw] w-full">
         <FloatingNav navItems={navItems} />
-        {isAdmin && <PassKeyModel  isAdmin={isAdmin}/>}
+        {isAdmin && <PassKeyModel isAdmin={isAdmin} />}
         <Hero />
       </div>
       <Grid />
