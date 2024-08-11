1022ff5b8aea487d00848116913df2b2768fdf20
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-08-11 18:02:45 +0530

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -13,32 +13,29 @@ import Footer from "./components/Footer";
 import PassKeyModel from "./components/PassKey";
 
 import { useEffect, useState } from 'react';
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { usePathname } from "next/navigation";
 import { getWorkExperience } from "@/app/lib/actions/admin.actions";
 
-export default function Home({searchParams}:any) {
+export default function Home() {
   const router = useRouter();
   const [isAdmin, setIsAdmin] = useState(false);
-  const pathname=usePathname();
-   
-  //const isAdmin=searchParams?.admin==="true";
+  const pathname = usePathname();
+  const searchParams = useSearchParams();  // Updated hook to fetch query params
 
   useEffect(() => {
-    const admin = searchParams?.admin=="true";
-    console.log("typeof admin",typeof admin)
-    console.log("admin",admin)
-    console.log("searchParams?.admin",searchParams?.admin)
+    const admin = searchParams.get('admin') === "true";  // Extract the query parameter
+    console.log("typeof admin", typeof admin);
+    console.log("admin", admin);
+    console.log("searchParams.get('admin')", searchParams.get('admin'));
     setIsAdmin(admin);
-  }, [isAdmin, pathname,searchParams]);
-
-
+  }, [pathname, searchParams]);
 
   return (
     <main className="relative bg-black justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-[100vw] w-full">
         <FloatingNav navItems={navItems} />
-        {isAdmin && <PassKeyModel  isAdmin={isAdmin}/>}
+        {isAdmin && <PassKeyModel isAdmin={isAdmin} />}
         <Hero />
       </div>
       <Grid />
